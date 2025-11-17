8e5ba4297d5a7fe9f006084900cca6317eff55d3
saradir <120840462+saradir@users.noreply.github.com>
2025-11-17 22:27:10 +0200

    initial commit

---
+++ db/pool.js
@@ -0,0 +1,4 @@
+const { Pool } = require('pg');
+
+module.exports = new Pool ({
+    connectionString: process.env.DATABASE_URL});
---
+++ db/populatedb.js
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+require("dotenv").config();
+const { Client } = require("pg");
+
+const SQL = `
