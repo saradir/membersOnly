f996454f203d1db851709b3bcb6a195e362b4cbe
saradir <120840462+saradir@users.noreply.github.com>
2025-11-17 23:21:15 +0200

    fix db init file. create .env file

--- db/init.js
+++ db/init.js
@@ -15,11 +15,31 @@ hashed_password TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS messages(
-id INTEGER PRIMARY KEY GENERATE ALWAYS AS IDENTITY,
+id INTEGER  GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 title TEXT NOT NULL,
 text TEXT NOT NULL,
-user_id INTEGER REFERENCES users(id) NOT NULL ON DELETE CASCADE
+user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
-`
+`;
+
+async function main() {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  try {
+    console.log("🌱 Seeding database...");
+    await client.connect();
+    await client.query(SQL);
+    console.log("✅ Done!");
+  } catch (err) {
+    console.error("❌ Error seeding database:", err);
+  } finally {
+    await client.end();
+  }
+}
+
+main();
+
